e27e73583039f8c57ad0b8b83b40da8ee921734d
Salvador Parajon <rcserna09@gmail.com>
2024-04-18 13:20:07 -0700

    Player control
    made a game managar

--- P#5SernaSoundandEffectUnit#3/Assets/Scripts/PlayerController.cs
+++ P#5SernaSoundandEffectUnit#3/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,10 @@ public class PlayerController : MonoBehaviour
     public AudioClip crashSound;
     public float jumpForce;
     public float gravityModifier;
+    public float doubleJumpForce;
     public bool isOnGround = true;
+    public bool doubleJumpUsed = false;
+    public bool doubleSpeed =false;
     public bool gameOver;
 
     // Start is called before the first frame update
@@ -36,6 +39,25 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound,1.0f);
+
+            doubleJumpUsed = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space)&& !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+        if (Input.GetKey(KeyCode.LeftShift)) 
+        { 
+            doubleSpeed = true; 
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f); 
+        } 
+        else if (doubleSpeed)
+        { 
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
         }
     }
 
